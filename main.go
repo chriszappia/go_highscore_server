c99857877f05a3a7fd47d609e07db3b5f4ce3461
Chris <christopher.zappia@gmail.com>
2022-07-01 23:48:25 +1000

    database

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite" // Sqlite driver based on GGO
+	"gorm.io/gorm"
 )
 
 type score struct {
@@ -13,9 +15,14 @@ type score struct {
 	Location string `json:"location"`
 }
 
-var scores = []score{}
+// var scores = []score{}
+var db *gorm.DB
 
 func main() {
+
+	db, _ = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	db.AutoMigrate(&score{})
+
 	router := gin.Default()
 	router.GET("/highscore", getScores)
 	router.POST("/highscore", addScore)
@@ -31,15 +38,11 @@ func getScores(c *gin.Context) {
 	start, _ := strconv.Atoi(startStr)
 	count, _ := strconv.Atoi(countStr)
 
-	if start > len(scores) {
-		c.IndentedJSON(http.StatusOK, []score{})
-		return
-	}
-	end := start + count
-	if (start + count) > len(scores) {
-		end = len(scores)
-	}
-	c.IndentedJSON(http.StatusOK, scores[start:end])
+	var scores []score
+
+	db.Debug().Order("Score desc").Limit(count).Offset(start).Find(&scores)
+
+	c.IndentedJSON(http.StatusOK, scores)
 }
 
 func addScore(c *gin.Context) {
@@ -49,18 +52,23 @@ func addScore(c *gin.Context) {
 		return
 	}
 
-	scores = append(scores, newScore)
+	db.Create(&newScore)
+
+	// scores = append(scores, newScore)
 	c.IndentedJSON(http.StatusCreated, newScore)
 }
 
 func getScoreByUsername(c *gin.Context) {
 	username := c.Param("username")
 
-	for _, s := range scores {
-		if s.Username == username {
-			c.IndentedJSON(http.StatusOK, s)
-			return
-		}
+	var score score
+	result := db.Where("Username = ?", username).First(&score)
+	if result.RowsAffected == 0 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Not found"})
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Not found"})
+
+	c.IndentedJSON(http.StatusOK, score)
+	return
 }
+
